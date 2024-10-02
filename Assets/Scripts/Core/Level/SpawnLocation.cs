5cd2af500f91bc2d508d0fd29328de706fb73cfd
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-10-02 15:45:31 +0330

    added wave generator prefab

---
+++ Assets/Scripts/Core/Level/SpawnLocation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLocation : MonoBehaviour
+{
+    public static Vector3 SpawnPosition(float xMax, float zMax, float xMin, float zMin, float y)
+    {
+        Camera camera = Camera.main;
+        float cameraY=camera.transform.position.y;
+        Vector3 cameraBottomLeft = camera.ViewportToWorldPoint(new Vector3(0, cameraY, 0));
+        Vector3 cameraTopRight = camera.ViewportToWorldPoint(new Vector3(1, cameraY, 1));
+
+        float xRight = UnityEngine.Random.Range(cameraTopRight.x, xMax);
+        float xLeft = UnityEngine.Random.Range(xMin, cameraBottomLeft.x);
+
+        float zTop = UnityEngine.Random.Range(cameraTopRight.z, zMax);
+        float zBottom = UnityEngine.Random.Range(zMin, cameraBottomLeft.z);
+
+
+        int choice = UnityEngine.Random.Range(0, 3);
+
+        switch (choice)
+        {
+            case 0:
+                return new Vector3(xRight, y, zTop);
+            case 1:
+                return new Vector3(xLeft, y, zBottom);
+            case 2:
+                return new Vector3(xLeft, y, zTop);
+            case 3:
+                return new Vector3(xRight, y, zBottom);
+
+            default:
+                return Vector3.zero;
+        }
+    }
+}
--- Assets/Scripts/Core/Level/WaveGenerator.cs
+++ Assets/Scripts/Core/Level/WaveGenerator.cs
@@ -1,5 +1,5 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
 
 namespace Level
@@ -28,9 +28,60 @@ namespace Level
 
         [Header("Enemies")]
         [SerializeField] private List<EnemySpawn> enemySpawnList=new List<EnemySpawn>();
+
+
+        [Header("Level Info")]
+        [SerializeField] private float xMin;
+        [SerializeField] private float xMax;
+        [SerializeField] private float zMax;
+        [SerializeField] private float zMin;
+        [SerializeField] private float enemyY;
+
         private List<int> _enemyCounts=new List<int>();
+        private int _sumOfRates;
+
 
         #endregion
+
+        private void Start()
+        {
+            foreach(EnemySpawn enemySpawn in enemySpawnList)
+            {
+                _enemyCounts.Add(0);
+            }
+            _sumOfRates = 0;
+            foreach (EnemySpawn spawn in enemySpawnList)
+            {
+                _sumOfRates += spawn.spawnRate;
+            }
+
+            _currentCount = intialSpawnNumber;
+            GenerateWave();
+
+        }
+
+        private void GenerateWave()
+        {
+            int i = 0;
+            foreach(EnemySpawn spawn in enemySpawnList)
+            {
+                _enemyCounts[i] = spawn.spawnRate * _currentCount / _sumOfRates;
+                i++;
+            }
+
+            StartCoroutine(SpawnEnemies(_enemyCounts));
+        }
+
+        private IEnumerator SpawnEnemies(List<int> _counts)
+        {
+            for(int i=0;i< _currentCount;i++)
+            {
+                int rand = UnityEngine.Random.Range(0, _counts.Count);
+                Vector3 spawnPosition = SpawnLocation.SpawnPosition(xMax, zMax, xMin, zMin, enemyY);
+                Instantiate(enemySpawnList[rand].prefab, spawnPosition, Quaternion.identity, null);
+                yield return new WaitForSeconds(spawnRate);
+            }
+        }
 
         private void OnEnemyDeath()
         {
