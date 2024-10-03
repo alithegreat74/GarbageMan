4453a24229d06e9c54ceda795de816c599c5e381
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-10-03 17:49:53 +0330

    added upgrade system

---
+++ Assets/Scripts/Core/ItemDrop/Item.cs
@@ -0,0 +1,41 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private float reachTime;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float lifeTime;
+
+    public Upgrade upgrade;
+    private void OnEnable()
+    {
+        StartCoroutine(FollowPlayer());        
+    }
+
+    private IEnumerator FollowPlayer()
+    {
+        float timer = 0;
+        while (timer <= reachTime)
+        {
+            timer += Time.deltaTime;
+            Vector3 lerp = Vector3.Lerp(transform.position, Player.instance.transform.position, timer / reachTime);
+            transform.position = lerp;
+            yield return null;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
+            return;
+        if (upgrade.type == UpgradeType.Health)
+            Player.instance.GetComponent<Stats>().Heal(upgrade.value);
+        else
+            upgrade.AddUpgrade();
+
+        StopAllCoroutines();
+        transform.position = new Vector3(1000000000, 1000000000, 100000000);
+    }
+}
